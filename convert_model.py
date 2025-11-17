01a13d21e2f772263feb4707b7c1e791918da63f
irvin <irvinb35@gmail.com>
2025-11-16 22:51:43 -0700

    pdm_student_dropped_model converted

--- convert_model.py
+++ convert_model.py
@@ -8,7 +8,9 @@ import tensorflow as tf
 
 #keras_model = tf.keras.models.load_model("saved_pdm_student_model.keras")
 
-keras_model = tf.keras.models.load_model("saved_env_student_model.keras")
+#keras_model = tf.keras.models.load_model("saved_env_student_model.keras")
+
+keras_model = tf.keras.models.load_model("saved_pdm_student_dropped_model.keras")
 
 converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
 
@@ -22,5 +24,5 @@ tf_lite_model = converter.convert()
 #with open('env_model.tflite', 'wb') as f:
 #    f.write(tf_lite_model)
 
-with open('env_student_model.tflite', 'wb') as f:
+with open('pdm_student_dropped_model.tflite', 'wb') as f:
     f.write(tf_lite_model)
